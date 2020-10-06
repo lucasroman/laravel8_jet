160e10218cf3d7a94828824f25ddb4113de64812
Lucas Roman <lucasroman@gmail.com>
2020-10-06 11:35:04 -0300

    Replace errors loop messages by @error directive
    Also reorder code.

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,80 +2,80 @@
 
 @section('content')
     <div class="panel-body">
-        {{-- Display validation errors --}}
-        {{-- @include('common.errors') --}}
 
-        {{-- New Task form --}}
-        <form action="" method="post">
-            @csrf
-
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
+        {{-- Task name --}}
+        <div class="form-group container">
+            <div class="card mt-4">
+                <div class="card-header">
+                    New Task
                 </div>
-            @endif
 
-            {{-- Task name --}}
-            <div class="form-group container">
-                <div class="card mt-4">
-                    <div class="card-header">
-                        New Task
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        {{-- New Task form --}}
+                        <form action="" method="post">
+                            @csrf
                             <label for="task" class="font-weight-bold">
                                 Task
                             </label>
-                            <input type="text" class="form-control" name="name"
+
+                            {{-- Input task field --}}
+                            <input type="text" class="form-control"
+                                name="name"
                                 id="task-name">
-                                <button type="submit" class=
-                                    "btn btn-outline-secondary mt-3">
-                                    <i class="fas fa-plus"></i> Add Task
-                                </button>
-                        </li>
-                    </ul>
-                </div>
-        </form>
 
-        @if ($tasks->isNotEmpty())
-            <div class="card mt-4">
-                <div class="card-header">
-                    Current Tasks
-                </div>
+                            {{-- Input error message --}}
+                            @error('name')
+                                <div class="alert alert-danger mt-3">
+                                    {{ $message }}
+                                </div>
+                            @enderror
 
-                <table class="table table-striped">
-                    <thead>
-                        <th>Task</th>
-                    </thead>
-
-                    <tbody>
-                        @foreach ($tasks as $task)
-                            <tr>
-                                <form action=
-                                    "{{route('task.delete', $task->id)}}"
-                                    method="post">
-                                    @csrf
-                                    @method("delete")
-
-                                    <td>{{ $task->name }}</td>
-                                    <td>
-                                        <button type="submit" class=
-                                            "btn btn-danger">
-                                            <i class="fas fa-trash-alt"></i>
-                                            Delete
-                                        </button>
-                                    </td>
-                                </form>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            <button type="submit" class=
+                                "btn btn-outline-secondary mt-3">
+                                <i class="fas fa-plus"></i> Add Task
+                            </button>
+                        </form>
+                    </li>
+                </ul>
             </div>
-        @endif
+
+            {{-- Show task list if there --}}
+            @if ($tasks->isNotEmpty())
+                <div class="card mt-4">
+                    <div class="card-header">
+                        Current Tasks
+                    </div>
+
+                    <table class="table table-striped">
+                        <thead>
+                            <th>Task</th>
+                        </thead>
+
+                        <tbody>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <form action=
+                                        "{{route('task.delete', $task->id)}}"
+                                        method="post">
+                                        @csrf
+                                        @method("delete")
+
+                                        <td>{{ $task->name }}</td>
+                                        <td>
+                                            <button type="submit" class=
+                                                "btn btn-danger">
+                                                <i class="fas fa-trash-alt"></i>
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </form>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
