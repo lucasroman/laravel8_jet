6afebba97c846405bad6c4504bd204922a5e9cd7
Lucas Roman <lucasroman@gmail.com>
2020-11-10 15:50:58 -0300

    Add book's validations

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,9 +9,11 @@ class BookController extends Controller
 {
     public function store()
     {
-        Book::create([
-            'title' => request('title'),
-            'author' => request('author'),
+        $data = request()->validate([
+            'title' => 'required',
+            'author' => 'required',
         ]);
+
+        Book::create($data);
     }
 }
