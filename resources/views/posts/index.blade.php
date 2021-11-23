8211f365ee73305d8e7c0b6057b5d231ca5893d3
Lucas Roman <lucasroman@gmail.com>
2021-11-23 11:51:49 -0300

    Update post view: to read component in app.js

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,24 @@
-<h3>Post index view</h3>
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>{{ config('app.name', 'Test') }}</title>
+        
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h3>Post index view</h3>
+        
+        <div id="todoComponent">Todo component here</div>
+        
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
 
-<div id="todoComponent">Todo component here</div>
-
-<script src="/js/app.js"></script>
