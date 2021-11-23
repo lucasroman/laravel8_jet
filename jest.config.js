18698375d95ef2144f8314fca0963a87b21ab756
Lucas Roman <lucasroman@gmail.com>
2021-11-23 11:50:04 -0300

    Delete unnecessary config jest file

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/*module.exports = {
-    testRegex: 'resources/components/__test__/.*.test.js$'
-}*/
-
-module.exports = {
-    presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  };
