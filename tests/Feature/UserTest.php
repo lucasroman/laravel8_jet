c6641d3e81cdc839eecccf7440fbf0bf5a3d096f
Lucas Roman <lucasroman@gmail.com>
2021-11-28 13:04:57 -0300

    Fix User test filename

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class UserTest.php extends TestCase
+class UserTest extends TestCase
 {
     /**
      * A basic feature test example.
@@ -19,4 +19,8 @@ class UserTest.php extends TestCase
 
         $response->assertStatus(200);
     }
+
+    // 1. Check post url and view exist
+    // 2. It must exist a 'create post' link
+    
 }
