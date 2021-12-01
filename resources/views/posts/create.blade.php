29141a433015885229d3d2e7b00af90a30c44e4f
Lucas Roman <lucasroman@gmail.com>
2021-12-01 10:07:31 -0300

    Add create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,6 +1,13 @@
-<h1>Create a new post</h1>
-Title
-<input>
-Text
-<Textarea></Textarea>
+<a href="{{ route('dashboard') }}">Back</a>
+
+<form method="POST" action="{{ route('posts.store') }}">
+    @csrf
+    <h1>Create a new post</h1>
+    Title
+    <input>
+    Text
+    <Textarea></Textarea>
+    <input type=submit>
+</form>
+
 
