698925ba2377c93754d7afa27f03522a2724876e
Lucas Roman <lucasroman@gmail.com>
2021-12-02 12:19:53 -0300

    Post table: add text column

--- database/migrations/2021_11_20_235315_create_posts_table.php
+++ database/migrations/2021_11_20_235315_create_posts_table.php
@@ -16,6 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('text');
             $table->string('owner');
             $table->timestamps();
         });
