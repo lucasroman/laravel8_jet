d9931a5adffd56fbdbcd340177593cbc051ac413
Lucas Roman <lucasroman@gmail.com>
2021-12-09 10:50:52 -0300

    PostController: add index action

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        // Current user logged
+        $user = auth()->user();
+
+        return view('posts.index', ['user' => $user]);
     }
 }
