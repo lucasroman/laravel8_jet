1b9dcd9925b80c369a481cc802356b79fcb8ed3b
Lucas Roman <lucasroman@gmail.com>
2021-12-13 12:23:58 -0300

    Add UserSeeder to DatabaseSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'name' => 'admin', 
+            'email' => 'admin@example.com', 
+            // Hash the password is necessary to login otherwise it will not work
+            'password' => Hash::make('12345678'), 
+        ]);
     }
 }
